32cf0be66315bf9801de8e5240083722e99afc42
Saeideh Nezhad <sepide.eslamnezhad@gmail.com>
2026-02-16 23:15:05 +0100

    add dynamic title to router

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,19 @@
-import { Outlet } from "react-router-dom";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+import { useEffect } from 'react';
+import { Outlet, useMatches } from 'react-router-dom';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 export default function RootLayout() {
+  const matches = useMatches();
+
+  useEffect(() => {
+    const handle = matches[matches.length - 1]?.handle as
+      | { title?: string }
+      | undefined;
+    const title = handle?.title || 'Sepezh | Full Stack Developer';
+    document.title = title;
+  }, [matches]);
+
   return (
     <>
       <Header />
